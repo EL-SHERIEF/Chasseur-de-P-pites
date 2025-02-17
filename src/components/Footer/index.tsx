5c6111b95ec71a13579eaded7bf910704574dc69
EL-SHERIEF <ahmed-elsherief@outlook.com>
2025-02-17 11:33:10 +0200

    done

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer
-      className="wow fadeInUp relative z-10 bg-[#090E34] pt-20 lg:pt-[100px]"
+      className="wow fadeInUp relative z-10 bg-[#222222] pt-20 lg:pt-[100px]"
       data-wow-delay=".15s"
     >
       <div className="container">
@@ -273,13 +273,6 @@ const Footer = () => {
       </div>
 
       <div>
-        <span className="absolute left-0 top-0 z-[-1] aspect-[95/82] w-full max-w-[570px]">
-          <Image src="/images/footer/shape-1.svg" alt="shape" fill />
-        </span>
-
-        <span className="absolute bottom-0 right-0 z-[-1] aspect-[31/22] w-full max-w-[372px]">
-          <Image src="/images/footer/shape-3.svg" alt="shape" fill />
-        </span>
 
         <span className="absolute right-0 top-0 z-[-1]">
           <svg
